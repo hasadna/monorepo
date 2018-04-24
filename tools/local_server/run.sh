032fc198fe58c873a04c73a59042449844f5c8ed
oferb <oferb@google.com>
2018-04-24 22:31:15 +0300

    Add web_login ng serve to run.sh. Also modify gitignores

--- tools/local_server/run.sh
+++ tools/local_server/run.sh
@@ -15,7 +15,15 @@ fi
 # Kill previous:
 pkill -f 'tools/local_server/local_http_gateway'
 pkill -f 'tools/local_server/local_server'
-sleep 0.5  # Wait for processes to terminate
+fuser -k 8000/tcp # For Angular
+
+# We clone StartupOS to run the local Angular server. Otherwise it won't work
+# (see https://github.com/alexeagle/angular-bazel-example/issues/122)
+git clone https://github.com/google/startup-os.git tools/local_server/startup-os
+cd tools/local_server/startup-os/tools/local_server/web_login
+npm install
+ng serve &
+cd -
 
 # Run:
 bazel-bin/external/startup_os/tools/local_server/local_http_gateway &
