6dc27254aef30a91be49d6eafff7a6e46f048c6b
Max Vorobev <vmax0770@gmail.com>
2018-07-12 13:27:28 +0300

    fix from review: compile.sh

--- compile.sh
+++ compile.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-# Builds all targets and runs all tests
-if [[ ! -z "$CIRCLECI" ]]; then
-  DELETED_PACKAGES="";
-else
-  DELETED_PACKAGES="--deleted_packages $(cat .circleci/deleted_bazel_packages.txt)";
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+
+# Run with `bazel` command as param (build|test)
+if [[ $1 != "build" && $1 != "test" ]]; then
+  echo "$RED""Run script with 'build' or 'test' as param$RESET"
+  exit 1
 fi
 
+# Builds all targets and runs all tests
 PROTO_OPTIONS="--proto_compiler //external:proto_compiler --proto_toolchain_for_java //external:proto_java_toolchain"
 
-time bazel build $PROTO_OPTIONS $DELETED_PACKAGES //...
+bazel $1 $PROTO_OPTIONS //...
 exit $?
