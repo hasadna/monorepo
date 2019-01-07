ba78d578b09d83cb0d071ed3bd8f4f96cacad359
Alon <alony60@gmail.com>
2019-01-07 10:45:52 +0200

    Added social networks icons in 'images' folder within 'assets'.

--- projects/mysadna2/src/app/user-details/user-details.component.ts
+++ projects/mysadna2/src/app/user-details/user-details.component.ts
@@ -9,7 +9,7 @@ import { DataService } from '../services/data.service';
 @Component({
   selector: 'app-user-details',
   templateUrl: './user-details.component.html',
-  styleUrls: ['./user-details.component.scss']
+  styleUrls: ['./user-details.component.scss'],
 })
 export class UserDetailsComponent implements OnInit {
   user: User;
