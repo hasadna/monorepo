dfee1bac05e3b39a947e0880e14ccef0748fbbef
Max Vorobev <vmax0770@gmail.com>
2019-01-14 23:34:07 +0300

    Exclude android_* targets if sdk is not available (#46)

--- compile.sh
+++ compile.sh
@@ -5,6 +5,18 @@ RESET=$(tput sgr0)
 
 CIRCLECI_MAX_ATTEMPTS=10
 
+function bazel_build() {
+  if [[ -z "$ANDROID_HOME" ]]; then
+    # Ignore third_party and android targets
+    bazel query '//... except //third_party/... except kind("android_.* rule", //...)' | xargs bazel $1
+    return $?
+  else
+    # Ignore just third_party
+    bazel query '//... except //third_party/...' | xargs bazel $1
+    return $?
+  fi
+}
+
 # Run with `bazel` command as param (build|test)
 if [[ $1 != "build" && $1 != "test" ]]; then
   echo "$RED""Run script with 'build' or 'test' as param$RESET"
@@ -26,5 +38,5 @@ if [[ ! -z "$CIRCLECI" ]]; then
   exit 1
 fi
 
-bazel $1 //...
+bazel_build $1
 exit $?
