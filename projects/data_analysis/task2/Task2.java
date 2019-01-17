d31158a53895966552c7fd66d189be3ec483e5b3
Max Vorobev <vmax0770@gmail.com>
2019-01-17 11:47:15 +0300

    fix //projects/data_analysis/task2:task2-checkstyle

--- projects/data_analysis/task2/Task2.java
+++ projects/data_analysis/task2/Task2.java
@@ -9,23 +9,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 class Task2 {
-    public static void main(String[] args) throws IOException {
-        Path file1 = Paths.get(args[0]);
-        Path outfile = Paths.get(args[1]);
+  public static void main(String[] args) throws IOException {
+    Path file1 = Paths.get(args[0]);
+    Path outfile = Paths.get(args[1]);
 
-        // read prototxt
-        String protoTxt = new String(Files.readAllBytes(file1));
+    // read prototxt
+    String protoTxt = new String(Files.readAllBytes(file1));
 
-        // parse into proto message instance
-        Users.Builder builder = Users.newBuilder();
-        TextFormat.merge(protoTxt, builder);
+    // parse into proto message instance
+    Users.Builder builder = Users.newBuilder();
+    TextFormat.merge(protoTxt, builder);
 
-        // count all users in parsed proto
-        String stats = String.format("Total number of users read: %d\n",
-                builder.build().getUsersCount());
+    // count all users in parsed proto
+    String stats = String.format("Total number of users read: %d\n",
+        builder.build().getUsersCount());
 
-        // write out stats for further processing
-        Files.write(outfile, stats.getBytes());
-    }
+    // write out stats for further processing
+    Files.write(outfile, stats.getBytes());
+  }
 }
 
