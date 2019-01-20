5a557149a808d5cdd4bc396e3a90dfa17faa92cc
yehudalev <ylev1988@gmail.com>
2019-01-20 10:20:54 +0200

    print to standart output the following format: file_name: number_of_lines

--- projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
+++ projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
@@ -4,28 +4,21 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 class DataHandler {
       public static void main(String[] args) throws IOException {
-
-        Path outfile = Paths.get(args[2]);
-        List<String> first_names = Files.readAllLines(Paths.get(args[0]));
-        List<String> last_names = Files.readAllLines(Paths.get(args[1]));
-
-        String str = first_names.get(0);
-        Files.write(outfile, str.getBytes());
-
-        /*
-        Path outfile = Paths.get(args[args.length]);
-        String str = "hello-world\n";
+        System.out.println(args[0]);
+        String outPut = "";
+        Path outfilePath = Paths.get(args[args.length-1]);
         for (String path : args) {
             Path curPath = Paths.get(path);
-            str = str + curPath.getFileName() + " lines: ";
-            List<String> curPathLines = Files.readAllLines(curPath);
-            str = str + String.valueOf(curPathLines.size());
+            List<String> curPathContent = Files.readAllLines(curPath);
+            outPut = outPut + curPath.getFileName() + " lines: ";
+            outPut = outPut + String.valueOf(curPathContent.size()) + "\n";
+            Files.write(outfilePath, curPathContent);
         }
-        Files.write(outfile, str.getBytes());
-        */
+        System.out.print(outPut);
     }
 }
