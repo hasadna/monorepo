61b957b095a7ef1a91ea3482e6a09724dcd50c03
LiadAzulay <35407568+LiadAzulay@users.noreply.github.com>
2019-01-22 21:04:26 +0200

    Update firebase.module.ts

--- projects/storyteller/webapp/src/app/import/firebase/firebase.module.ts
+++ projects/storyteller/webapp/src/app/import/firebase/firebase.module.ts
@@ -3,9 +3,8 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFirestoreModule, FirestoreSettingsToken } from 'angularfire2/firestore';
-
 import { config } from './config';
-import { FirestoreSettingsToken } from '@angular/fire/firestore';
+
 @NgModule({
   imports: [
     AngularFireModule.initializeApp(config),
