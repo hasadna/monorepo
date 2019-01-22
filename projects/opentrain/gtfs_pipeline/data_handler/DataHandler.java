c7a03db669e4966e26b934d3fc5f204a4e311d5d
yehudalev <ylev1988@gmail.com>
2019-01-22 20:39:18 +0200

    write the file names and number of lines in the file

--- projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
+++ projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
@@ -18,8 +18,8 @@ class DataHandler {
             List<String> curPathContent = Files.readAllLines(curPath);
             output += curPath.getFileName() + " lines: ";
             output += curPathContent.size() + "\n";
-            Files.write(outputPath, new String(curPath.getFileName()+"\n").getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-            Files.write(outputPath, curPathContent, StandardOpenOption.APPEND);
+            Files.write(outputPath, new String(curPath.getFileName()+"\t").getBytes(), StandardOpenOption.APPEND, StandardOpenOption.CREATE);
+            Files.write(outputPath, new String("lines: " + curPathContent.size()).getBytes(), StandardOpenOption.APPEND);
             Files.write(outputPath, new String("\n").getBytes(), StandardOpenOption.APPEND);
 
         }
