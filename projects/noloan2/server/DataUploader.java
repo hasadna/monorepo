b49d56b5161704a824aa60dc0838e5b91d90298b
gal golan <galgolo@gmail.com>
2019-01-29 03:43:17 -0800

    WIP DataUploader tool

--- projects/noloan2/server/DataUploader.java
+++ projects/noloan2/server/DataUploader.java
@@ -2,37 +2,55 @@ package hasadna.projects.noloan2.server;
 
 import com.google.startupos.common.CommonModule;
 import com.google.startupos.common.FileUtils;
-import com.google.startupos.common.flags.Flag;
-import com.google.startupos.common.flags.FlagDesc;
 import com.google.startupos.common.flags.Flags;
+import com.google.startupos.tools.localserver.service.AuthService;
 import dagger.Component;
-import java.io.IOException;
-import java.util.Base64;
+import hasadna.noloan2.protobuf.SMSProto.SpamList;
+import hasadna.noloan2.protobuf.SMSProto.SmsMessage;
+
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.IOException;
 
 @Singleton
 public class DataUploader {
 
-  private FileUtils fileUtils;
+    private final String SPAM_DOCUMENT_PATH = "noloan/smss";
 
-  @Inject
-  DataUploader(FileUtils fileUtils) {
-    this.fileUtils = fileUtils;
-  }
+    private FileUtils fileUtils;
+    private AuthService authService;
 
-  void run() throws IOException {
-    //TODO Implement
-  }
+    @Inject
+    DataUploader(FileUtils fileUtils, AuthService authService) {
+        this.fileUtils = fileUtils;
+        this.authService = authService;
+    }
 
-  @Singleton
-  @Component(modules = CommonModule.class)
-  interface ToolComponent {
-    DataUploader getTool();
-  }
+    void run() throws IOException {
+        SmsMessage sms1 = SmsMessage.newBuilder().setSender("mobile").setBody("ההלוואה הכי מהירה שיש עד 20 אלף שח אצלך ביד, החזר רק בצקים 023750707").build();
+        SmsMessage sms2 = SmsMessage.newBuilder().setSender("mobile").setBody("בהמשך לפנייתך להלוואה נא לחץ על הקישור למילוי הפרטים לזרז הטיפול בפנייתך").build();
+        SmsMessage sms3 = SmsMessage.newBuilder().setSender("Yeah-Atid").setBody("ח\"כ מיקי לוי מגיע לחיפה לדבר איתכם על כל מה שבוער במדינה. יום א' (4/3) בשעה 19:30 בפאב \"סטלה\", רחוב הירקון 1 פינת מוריה, חיפה. להרשמה ופרטים נוספים: https://goo.gl/tzTVPc  להסרה: https://goo.gl/QjbrTs023750707").build();
+        SpamList spam = SpamList.newBuilder().addSms(sms1).addSms(sms2).addSms(sms3).build();
+        
+        fileUtils.writePrototxt(spam, "spam.Prototxt");
 
-  public static void main(String[] args) throws IOException {
-    Flags.parseCurrentPackage(args);
-    DaggerDataUploader_ToolComponent.create().getTool().run();
-  }
+        /* authService.refreshToken();
+        FirestoreProtoClient client = new FirestoreProtoClient(authService.getProjectId(), authService.getToken());
+        SpamList spam = (SpamList) fileUtils.readPrototxt("Spam.prototxt", SpamList.newBuilder());
+        List<SmsMessage> SMSList = spam.getSms();
+
+      client.setProtoDocument(SPAM_DOCUMENT_PATH, spam);*/
+    }
+
+    @Singleton
+    @Component(modules = CommonModule.class)
+    interface ToolComponent {
+        DataUploader getTool();
+    }
+
+    public static void main(String[] args) throws IOException {
+        Flags.parseCurrentPackage(args);
+        DaggerDataUploader_ToolComponent.create().getTool().run();
+    }
 }
