613f9ebf24beeea33d38a3762d5be55b5eacf9e0
Web Developland <webdevelopland@protonmail.com>
2019-01-29 16:09:25 +0300

    code review

---
+++ projects/mysadna/src/app/components/index.ts
@@ -0,0 +1,16 @@
+export * from './project-details';
+export * from './projects';
+export * from './user-details';
+export * from './users';
+
+import { ProjectDetailsComponent } from './project-details';
+import { ProjectsComponent } from './projects';
+import { UserDetailsComponent } from './user-details';
+import { UsersComponent } from './users';
+
+export const ComponentList = [
+  ProjectDetailsComponent,
+  ProjectsComponent,
+  UserDetailsComponent,
+  UsersComponent,
+];
