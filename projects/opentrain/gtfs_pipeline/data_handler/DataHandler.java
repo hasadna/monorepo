911977d1449519cddd08c94a97ec897799987ae5
Yehudalev <ylev1988@gmail.com>
2019-01-31 12:07:27 +0200

    update main

--- projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
+++ projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
@@ -30,66 +30,31 @@ import java.util.List;
 import java.lang.String;
 import static java.nio.charset.StandardCharsets.UTF_8;
 class DataHandler {
-  
-    //method "getStops" for protobuf the file stops.txt  
-    public static Stops.Builder getStops(Path csvPath) {
-    Stops.Builder allStopsBuilder = Stops.newBuilder();
-    try {
-      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+  public static void main(String[] args) throws IOException {
+    // args[7] is stops.txt
+      Stops.Builder allStopsBuilder = Stops.newBuilder();
+      Path stops_txt = Paths.get(args[7]);
+      Path outputPath = Paths.get(args[args.length - 1]);
+      CSVParser parser = CSVParser.parse(stops_txt, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
       List<CSVRecord> csvRecords = parser.getRecords();
 
-      //remove the first record which are columns title
-      csvRecords.remove(csvRecords.get(0));
-      //iterate each record, create a message with builder
-      for (CSVRecord record : csvRecords) {
-        /*System.out.println(record.get(0));
-        System.out.println(record.get(1));
-        System.out.println(record.get(2));
-        System.out.println(record.get(3));
-        System.out.println(record.get(4));
-        System.out.println(record.get(5));
-        System.out.println(record.get(6));
-        System.out.println(record.get(7));
-        System.out.println(record.get(8));*/
-        allStopsBuilder.addStops(Stop.newBuilder()
-                .setStopId(Integer.parseInt(record.get(0)))
-                .setStopCode(Integer.parseInt(record.get(1)))
-                .setStopName(record.get(2))
-                .setStopDesc(record.get(3))
-                .setStopLat(Double.parseDouble(record.get(4)))
-                .setStopLon(Double.parseDouble(record.get(5)))
-                .setLocationType(Integer.parseInt(record.get(6)))
-                .setParentStation(record.get(7))
-                .setZoneId(Integer.parseInt(record.get(8)))
-                .build());
-      }
+      //remove the first record from the list
+       csvRecords.remove(csvRecords.get(0));
 
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-
-    return allStopsBuilder;
+       //iterate each record, create a message with builder
+       for (CSVRecord record : csvRecords) {
+         allStopsBuilder.addStops(Stop.newBuilder()
+                 .setStopId(Integer.parseInt(record.get(0)))
+                 .setStopCode(Integer.parseInt(record.get(1)))
+                 .setStopName(record.get(2))
+                 .setStopDesc(record.get(3))
+                 .setStopLat(Double.parseDouble(record.get(4)))
+                 .setStopLon(Double.parseDouble(record.get(5)))
+                 .setLocationType(Integer.parseInt(record.get(6)))
+                 .setParentStation(record.get(7))
+                 .setZoneId(Integer.parseInt(record.get(8)))
+                 .build());
+       }
+      Files.write(outputPath, TextFormat.printToString(allStopsBuilder).getBytes());
   }
-
-
-
-
-      public static void main(String[] args) throws IOException {
-        
-        //args[7] is stops.txt
-        Path stops_txt = Paths.get(args[7]);
-
-
-
-
-        //String output = "";
-        Path outputPath = Paths.get(args[args.length-1]);
-        //for (String path : args) {
-            // TODO: Read the files in a streaming manner since they are large
-           // Path curPath = Paths.get(path);
-           // output += curPath.getFileName() + " lines: ";
-           Files.write(outputPath, TextFormat.printToString(getStops(stops_txt)).getBytes());
-           // }
-       // System.out.print(output);
-    }
 }
