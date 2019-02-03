ba7d8e82c6fc50384424e85959327d7edf010d70
Yehudalev <ylev1988@gmail.com>
2019-02-03 22:38:45 +0200

    added more csv files to proto functions

--- projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
+++ projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
@@ -6,6 +6,16 @@ import com.projects.opentrain.gtfs_pipeline.AgencyProtos.Agency;
 import com.projects.opentrain.gtfs_pipeline.AgencyProtos.Agencies;
 import com.projects.opentrain.gtfs_pipeline.CalendarProtos.Calendar;
 import com.projects.opentrain.gtfs_pipeline.CalendarProtos.Calendars;
+import com.projects.opentrain.gtfs_pipeline.FareAttributesProtos.FareAttributes;
+import com.projects.opentrain.gtfs_pipeline.FareAttributesProtos.FaresAttributes;
+import com.projects.opentrain.gtfs_pipeline.FareRulesProtos.FareRules;
+import com.projects.opentrain.gtfs_pipeline.FareRulesProtos.FaresRules;
+import com.projects.opentrain.gtfs_pipeline.RoutesProtos.Route;
+import com.projects.opentrain.gtfs_pipeline.RoutesProtos.Routes;
+import com.projects.opentrain.gtfs_pipeline.ShapesProtos.Shape;
+import com.projects.opentrain.gtfs_pipeline.ShapesProtos.Shapes;
+import com.projects.opentrain.gtfs_pipeline.StopTimesProtos.StopTimes;
+import com.projects.opentrain.gtfs_pipeline.StopTimesProtos.StopsTimes;
 import com.projects.opentrain.gtfs_pipeline.StopsProtos.Stop;
 import com.projects.opentrain.gtfs_pipeline.StopsProtos.Stops;
 import com.google.protobuf.TextFormat;
@@ -79,6 +89,105 @@ class DataHandler {
     return allCalendarsBuilder;
   }
 
+  public static FaresAttributes.Builder getFareAttributes(Path csvPath) {
+    FaresAttributes.Builder allFaresAttributesBuilder = FaresAttributes.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allFaresAttributesBuilder.addFaresattributes(
+            FareAttributes.newBuilder().setFareId(Integer.parseInt(record.get(0)))
+			.setPrice(Double.parseDouble(record.get(1))).setCurrencyType(record.get(2))
+			.setPaymentMethod(Integer.parseInt(record.get(3))).setTransfers(Integer.parseInt(record.get(4)))
+			.setAgencyId(Integer.parseInt(record.get(5))).setTransferDuration(record.get(6)).build());
+      }
+    } catch (IOException e) {
+    }
+    return allFaresAttributesBuilder;
+  }
+
+  public static FaresRules.Builder getFareRules(Path csvPath) {
+    FaresRules.Builder allFaresRulesBuilder = FaresRules.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allFaresRulesBuilder.addFaresrules(
+            FareRules.newBuilder().setFareId(Integer.parseInt(record.get(0)))
+			.setRouteId(Integer.parseInt(record.get(1))).setOriginId(Integer.parseInt(record.get(2)))
+			.setDestinationId(Integer.parseInt(record.get(3))).setContainsId(record.get(4)).build());
+      }
+    } catch (IOException e) {
+    }
+    return allFaresRulesBuilder;
+  }
+
+  public static Routes.Builder getRoutes(Path csvPath) {
+    Routes.Builder allRoutesBuilder = Routes.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allRoutesBuilder.addRoutes(
+            Route.newBuilder().setRouteId(Integer.parseInt(record.get(0))).setAgencyId(Integer.parseInt(record.get(1)))
+                .setRouteShortName(record.get(2)).setRouteLongName(record.get(3)).setRouteDesc(record.get(4))
+                .setRouteType(Integer.parseInt(record.get(5))).setRouteColor(record.get(6)).build());
+      }
+    } catch (IOException e) {
+    }
+    return allRoutesBuilder;
+  }
+
+  public static Shapes.Builder getShapes(Path csvPath) {
+    Shapes.Builder allShapesBuilder = Shapes.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allShapesBuilder.addShapes(
+            Shape.newBuilder().setShapeId(Integer.parseInt(record.get(0))).setShapePtLat(Double.parseDouble(record.get(1)))
+            .setShapePtLot(Double.parseDouble(record.get(2))).setShapePtSequence(Integer.parseInt(record.get(3))).build());
+      }
+    } catch (IOException e) {
+    }
+    return allShapesBuilder;
+  }
+
+  public static StopsTimes.Builder getstoptimes(Path csvPath) {
+    StopsTimes.Builder allstopstimesBuilder = StopsTimes.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allstopstimesBuilder.addStopstimes(
+            StopTimes.newBuilder().setTripId(record.get(0)).setArrivalTime(record.get(1))
+                .setDepartureTime(record.get(2)).setStopId(Integer.parseInt(record.get(3))).setStopSequence(Integer.parseInt(record.get(4)))
+                .setPickupType(Integer.parseInt(record.get(5))).setDropOffType(Integer.parseInt(record.get(6)))
+                .setShapeDistTraveled(Integer.parseInt(record.get(7))).build());
+      }
+    } catch (IOException e) {
+    }
+    return allstopstimesBuilder;
+  }
+
   public static Stops.Builder getStops(Path csvPath) {
     Stops.Builder allStopsBuilder = Stops.newBuilder();
     try {
@@ -101,11 +210,15 @@ class DataHandler {
   }
 
   public static void main(String[] args) throws IOException {
-    //[0]=agency.txt, [1]=calendar.txt, [7]=stops.txt
     Path agency_txt = Paths.get(args[0]);
     Path calendar_txt = Paths.get(args[1]);
+    Path fare_attributes_txt = Paths.get(args[2]);
+    Path fare_rules_txt = Paths.get(args[3]);
+    Path routes_txt = Paths.get(args[4]);
+    Path shapes_txt = Paths.get(args[5]); 
+    Path stop_times_txt = Paths.get(args[6]);
     Path stops_txt = Paths.get(args[7]);
     Path outputPath = Paths.get(args[args.length - 1]);
-    Files.write(outputPath, TextFormat.printToString(getCalendar(calendar_txt)).getBytes());
+    Files.write(outputPath, TextFormat.printToUnicodeString(getstoptimes(stop_times_txt)).getBytes(UTF_8));
   }
 }
