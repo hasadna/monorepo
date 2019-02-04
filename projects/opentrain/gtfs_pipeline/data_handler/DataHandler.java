90d90b4433f855a6b938b7495ded03262037d39d
Yehudalev <ylev1988@gmail.com>
2019-02-04 19:37:20 +0200

    added the last 2 function convert to proto and added some comments

--- projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
+++ projects/opentrain/gtfs_pipeline/data_handler/DataHandler.java
@@ -18,6 +18,10 @@ import com.projects.opentrain.gtfs_pipeline.StopTimesProtos.StopTimes;
 import com.projects.opentrain.gtfs_pipeline.StopTimesProtos.StopsTimes;
 import com.projects.opentrain.gtfs_pipeline.StopsProtos.Stop;
 import com.projects.opentrain.gtfs_pipeline.StopsProtos.Stops;
+import com.projects.opentrain.gtfs_pipeline.TranslationsProtos.Translation;
+import com.projects.opentrain.gtfs_pipeline.TranslationsProtos.Translations;
+import com.projects.opentrain.gtfs_pipeline.TripsProtos.Trip;
+import com.projects.opentrain.gtfs_pipeline.TripsProtos.Trips;
 import com.google.protobuf.TextFormat;
 import java.io.Reader;
 
@@ -50,9 +54,10 @@ class DataHandler {
   public static Agencies.Builder getAgency(Path csvPath) {
     Agencies.Builder allAgenciesBuilder = Agencies.newBuilder();
     try {
+      //Creates a parser for the given path (csvPath argument)
       CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      //A list of CSVrecordes which each one represent a row in the csv file
       List<CSVRecord> csvRecords = parser.getRecords();
-
       // remove the first record which are columns title
       csvRecords.remove(csvRecords.get(0));
       // iterate each record, create a message with builder
@@ -167,10 +172,12 @@ class DataHandler {
     return allShapesBuilder;
   }
 
-  public static StopsTimes.Builder getstoptimes(Path csvPath) {
+  public static StopsTimes.Builder getStopTimes(Path csvPath) {
     StopsTimes.Builder allstopstimesBuilder = StopsTimes.newBuilder();
     try {
+      //Creates a parser for the given path (csvPath argument)      
       CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      //A list of CSVrecordes which each one represent a row in the csv file
       List<CSVRecord> csvRecords = parser.getRecords();
 
       // remove the first record which are columns title
@@ -208,17 +215,61 @@ class DataHandler {
     }
     return allStopsBuilder;
   }
+  
+  public static Translations.Builder getTranslations(Path csvPath) {
+    Translations.Builder allTranslationsBuilder = Translations.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        System.out.println(record);
+        allTranslationsBuilder.addTranslations(
+            Translation.newBuilder().setTransId(record.get(0)).setLang(record.get(1))
+                .setTranslation(record.get(2)).build());
+      }
+    } catch (IOException e) {
+    }
+    return allTranslationsBuilder;
+  }
+
+  public static Trips.Builder getTrips(Path csvPath) {
+    Trips.Builder allTripsBuilder = Trips.newBuilder();
+    try {
+      CSVParser parser = CSVParser.parse(csvPath, StandardCharsets.UTF_8, CSVFormat.DEFAULT);
+      List<CSVRecord> csvRecords = parser.getRecords();
+
+      // remove the first record which are columns title
+      csvRecords.remove(csvRecords.get(0));
+      // iterate each record, create a message with builder
+      for (CSVRecord record : csvRecords) {
+        allTripsBuilder.addTrips(
+            Trip.newBuilder().setRouteId(Integer.parseInt(record.get(0))).setServiceId(Integer.parseInt(record.get(1)))
+                .setTripId(record.get(2)).setTripHeadsign(record.get(3)).setDirectionId(Integer.parseInt(record.get(4)))
+                .setShapeId(record.get(5)).build());
+      }
+    } catch (IOException e) {
+    }
+    return allTripsBuilder;
+  }
 
   public static void main(String[] args) throws IOException {
+    //Path
     Path agency_txt = Paths.get(args[0]);
     Path calendar_txt = Paths.get(args[1]);
     Path fare_attributes_txt = Paths.get(args[2]);
     Path fare_rules_txt = Paths.get(args[3]);
     Path routes_txt = Paths.get(args[4]);
-    Path shapes_txt = Paths.get(args[5]); 
-    Path stop_times_txt = Paths.get(args[6]);
+    Path shapes_txt = Paths.get(args[5]);       //didn't work
+    Path stop_times_txt = Paths.get(args[6]);  //didn't work
     Path stops_txt = Paths.get(args[7]);
+    Path translations_txt = Paths.get(args[8]); //didn't work
+    Path trips_txt = Paths.get(args[9]);
+    //last argument is the output path
     Path outputPath = Paths.get(args[args.length - 1]);
-    Files.write(outputPath, TextFormat.printToUnicodeString(getstoptimes(stop_times_txt)).getBytes(UTF_8));
+    //Save the proto which returned from the "get*Somthing*" function to the output path as belew
+    Files.write(outputPath, TextFormat.printToUnicodeString(getStopTimes(stop_times_txt)).getBytes(UTF_8));
   }
 }
