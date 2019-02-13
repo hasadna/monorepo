165e4388830419e33128f47dbb645fb8423aaf99
root <root@localhost.localdomain>
2019-02-13 19:49:27 +0000

    adding Id and Author to Story

--- projects/storyteller/webapp/src/app/app.module.ts
+++ projects/storyteller/webapp/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FirebaseModule } from './import/firebase/firebase.module';
+import { HeaderComponent } from './components/header/header.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    UserInfoComponent,
+    FeedComponent,
   ],
   imports: [
     BrowserModule,
     CoreModule,
     FirebaseModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/storyteller/webapp/src/app/components/feed/feed.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { StoryList, Story } from '@/core/proto';
+import { EncodingService, FirebaseService } from '@/core/services';
+
+import { from } from 'rxjs';
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss']
+})
+export class FeedComponent implements OnInit {
+  storylist: StoryList[];
+  constructor(
+    // private encodingService: EncodingService,
+    private firebaseService: FirebaseService,
+    private encodingService: EncodingService,
+  ) {
+    if (firebaseService.isOnline) {
+      this.load();
+    } else {
+      this.firebaseService.anonymousLogin().then(() => {
+        this.load();
+      });
+    }
+  }
+
+  load(): void {
+    this.firebaseService.getstorylistAll().subscribe(storylist => {
+      this.storylist = storylist;
+
+    });
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+
