0d2293c7241915668f4dc54e95798f8af5c33c75
OzLustig <ozlustig@gmail.com>
2019-02-13 16:09:21 +0200

    Removed program-website in order to add it back. x

--- projects/program-website/src/app/import/firebase/config.ts
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: 'AIzaSyAWwTuTi9P6Fy0iK_Ux2R5LWUkFgN7lhEQ',
-  authDomain: 'protobin-reader.firebaseapp.com',
-  databaseURL: 'https://protobin-reader.firebaseio.com',
-  projectId: 'protobin-reader',
-  storageBucket: 'protobin-reader.appspot.com',
-  messagingSenderId: '229513278474',
-};
