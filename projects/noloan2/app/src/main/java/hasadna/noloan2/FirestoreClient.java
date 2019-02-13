b323141a3bee561e84b08b2206ba08920cf7452d
dd <ylev1988@gmail.com>
2019-02-13 20:23:46 +0200

    formatted

--- projects/noloan2/app/src/main/java/hasadna/noloan2/FirestoreClient.java
+++ projects/noloan2/app/src/main/java/hasadna/noloan2/FirestoreClient.java
@@ -21,40 +21,39 @@ import java.util.concurrent.ExecutionException;
 import hasadna.noloan2.protobuf.SMSProto.SpamList;
 import hasadna.noloan2.protobuf.SMSProto.SmsMessage;
 
-
 public class FirestoreClient {
   private final String SPAM_DOCUMENT_PATH = "noloan/smss";
-  
+
   // < - 22 > - 31 for removing and changing with the user name
-  private final String USER_SUGGEST_COLLECTION = "noloan/user_data/user/<username>/spam_suggestions";
-  
+  private final String USER_SUGGEST_COLLECTION =
+      "noloan/user_data/user/<username>/spam_suggestions";
+
   private FirebaseFirestore client;
-  
+
   public FirestoreClient() {
     client = FirebaseFirestore.getInstance();
   }
-  
+
   // Write the message to the Firestore
   public void writeMessage(SmsMessage message) {
     FirestoreElement element = encodeMessage(message);
-    
+
     // TODO add code to remove the <username> from the path and add the actual user name
-    
+
     client.collection(USER_SUGGEST_COLLECTION).add(element);
   }
-  
+
   public SpamList getSpam() {
     DocumentReference document = client.document(SPAM_DOCUMENT_PATH);
     SpamList spam = decodeSpam(document.get().getResult().toObject(FirestoreElement.class));
     return spam;
   }
-  
-  
+
   // Not in use, there for future reference
   /* QuerySnapshot snapshot;
   public ArrayList<SmsMessage> getMessages() {
     ArrayList<SmsMessage> results = new ArrayList<>();
-    
+
     Task<QuerySnapshot> task = client.get();
     task.addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
       @Override
@@ -66,13 +65,13 @@ public class FirestoreClient {
         }
       }
     });
-    
+
     try {
       snapshot = Tasks.await(task);
     } catch (ExecutionException | InterruptedException e) {
       Log.e("Firesotre", "Wait interrupted");
     }
-    
+
     if (snapshot != null) {
       List<DocumentSnapshot> list = snapshot.getDocuments();
       for (DocumentSnapshot document : list) {
@@ -85,14 +84,14 @@ public class FirestoreClient {
       return null;
     }
   }*/
-  
+
   // Encode user proto to base64 for storing in Firestore
   private FirestoreElement encodeMessage(SmsMessage message) {
     byte[] protoBytes = message.toByteArray();
     String base64BinaryString = Base64.encodeToString(protoBytes, Base64.DEFAULT);
     return new FirestoreElement(base64BinaryString);
   }
-  
+
   private SmsMessage decodeMessage(FirestoreElement element) {
     byte[] messageBytes = Base64.decode(element.getBase64(), Base64.DEFAULT);
     SmsMessage message = null;
@@ -103,7 +102,7 @@ public class FirestoreClient {
     }
     return message;
   }
-  
+
   // TODO combine the two decoders
   private SpamList decodeSpam(FirestoreElement element) {
     byte[] messageBytes = Base64.decode(element.getBase64(), Base64.DEFAULT);
@@ -116,3 +115,4 @@ public class FirestoreClient {
     return spam;
   }
 }
+
