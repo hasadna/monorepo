88521cca0c08f2da3c575de7979d02d73b03aa9a
OzLustig <ozlustig@gmail.com>
2019-02-26 15:16:45 +0200

    routing branch v3

--- projects/program-website/src/app/app.module.ts
+++ projects/program-website/src/app/app.module.ts
@@ -4,35 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { FirebaseModule } from './import/firebase/firebase.module';
-<<<<<<< HEAD
 import { AboutComponent } from './about-component/about.component';
 import { ProjectsComponent } from './projects-component/projects.component';
 import { ParticipantsComponent } from './participants-component/participants.component';
 import { ContactUsComponent } from './contact-us-component/contact-us.component';
 import {RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-=======
->>>>>>> c5bb66de788bac5bc3f1e955a0c7eb234ea340d2
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     AboutComponent,
     ProjectsComponent,
     ParticipantsComponent,
     ContactUsComponent,
-=======
->>>>>>> c5bb66de788bac5bc3f1e955a0c7eb234ea340d2
   ],
   imports: [
     BrowserModule,
     CoreModule,
     FirebaseModule,
-<<<<<<< HEAD
     AppRoutingModule
-=======
->>>>>>> c5bb66de788bac5bc3f1e955a0c7eb234ea340d2
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/program-website/src/app/import/firebase/firebase.module.ts
+++ projects/program-website/src/app/import/firebase/firebase.module.ts
@@ -1,15 +1,8 @@
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireAuth, AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireDatabase } from 'angularfire2/database';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-=======
 import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
->>>>>>> c5bb66de788bac5bc3f1e955a0c7eb234ea340d2
 
 import { config } from './config';
 
@@ -23,10 +16,7 @@ import { config } from './config';
   providers: [
     AngularFireDatabase,
     AngularFireAuth,
-<<<<<<< HEAD
-=======
     { provide: FirestoreSettingsToken, useValue: {} },
->>>>>>> c5bb66de788bac5bc3f1e955a0c7eb234ea340d2
   ],
   exports: [
     AngularFirestoreModule,
