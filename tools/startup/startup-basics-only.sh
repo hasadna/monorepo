1a63681130c89933ea201db37afa8a1acb784f45
val-fed <valerii.fedorenko.ua@gmail.com>
2019-03-14 10:49:04 +0200

    Add git installing

--- tools/startup/startup-basics-only.sh
+++ tools/startup/startup-basics-only.sh
@@ -4,6 +4,14 @@ readonly DOWNLOAD_DIR_NAME="downloads_$(date +%Y%m%d_%H%M%S)"
 readonly DOWNLOAD_DIR_ABS_PATH="$(pwd)/$DOWNLOAD_DIR_NAME"
 readonly BASE_ABS_PATH=${HOME}/base
 
+sudo apt update
+echo "Checking for git installing"
+if [[ $(which git; echo $?) == "1" ]]; then
+    echo "Installing git"
+    sudo apt install git
+    echo "git is installed"
+fi
+
 echo "Checking for curl installing"
 if [[ $(which curl; echo $?) == "1" ]]; then
     echo "Installing curl"
@@ -21,27 +29,30 @@ fi
 echo "Checking for JDK"
 if [[ $(which javac; echo $?) == "1" ]]; then
     echo "Installing JDK"
-    sudo apt update
     sudo apt-get install default-jdk
     echo "JDK is installed"
 fi
 
 mkdir ${DOWNLOAD_DIR_NAME}
+cd ${DOWNLOAD_DIR_ABS_PATH}
 
 # Install Chrome
 if [[ $(which google-chrome; echo $?) == "1" ]]; then
   echo "Installing Chrome"
-  wget -q -O ${DOWNLOAD_DIR_NAME}/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-  sudo dpkg -i ${DOWNLOAD_DIR_NAME}/google-chrome-stable_current_amd64.deb
+  wget -q -O google-chrome-stable_current_amd64.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  sudo dpkg -i google-chrome-stable_current_amd64.deb
   sudo apt-get -f install
 fi
 
 # Install Bazel
 if [[ $(which bazel; echo $?) == "1" ]]; then
+    readonly BAZEL_VERSION=0.21.0
     echo "Installing bazel"
-    wget -O ${DOWNLOAD_DIR_NAME}/bazel.0.21.0.sh https://github.com/bazelbuild/bazel/releases/download/0.21.0/bazel-0.21.0-installer-linux-x86_64.sh
-    chmod +x ${DOWNLOAD_DIR_NAME}/bazel.0.21.0.sh
-    sudo bash ${DOWNLOAD_DIR_NAME}/bazel.0.21.0.sh --user
+    sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python
+    wget -O "bazel.${BAZEL_VERSION}.sh" "https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/bazel-${BAZEL_VERSION}-installer-linux-x86_64.sh"
+    chmod +x "bazel.${BAZEL_VERSION}.sh"
+    sudo ./bazel.${BAZEL_VERSION}.sh --user
+    export PATH="$PATH:$HOME/bin"
     echo "bazel is installed"
 fi
 
