593c5422fd5c3db6101872393ba5d55a98c58e50
Jenia <jzalomsky93@gmail.com>
2019-03-16 16:31:01 +0200

    Created the line count pipeline

---
+++ projects/data_analysis/task3/Task3.java
@@ -0,0 +1,24 @@
+package projects.data_analysis.task3;
+
+import com.google.common.collect.ImmutableList;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+public class Task3 {
+    public static void main(String[] args) throws IOException {
+            ImmutableList<Path> paths = ImmutableList.of(Paths.get(args[0]), Paths.get(args[1]), Paths.get(args[2]));
+            Path outfile = Paths.get(args[3]);
+            StringBuilder result = new StringBuilder();
+            for(Path path:paths){
+                result.append(path.toString());
+                result.append(":");
+                result.append( Files.readAllLines(path).size());
+                result.append( '\n');
+            }
+            Files.write(outfile, result.toString().getBytes());
+    }
+}
