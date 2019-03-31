defc877691794b8ba4290520dcdadad6dc45ceca
Jenia <jzalomsky93@gmail.com>
2019-03-31 22:42:15 +0300

    4 files

--- projects/data_analysis/linecount/LineCount.java
+++ projects/data_analysis/linecount/LineCount.java
@@ -7,18 +7,18 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-
 public class LineCount {
-    public static void main(String[] args) throws IOException {
-            ImmutableList<Path> paths = ImmutableList.of(Paths.get(args[0]), Paths.get(args[1]), Paths.get(args[2]));
-            Path outfile = Paths.get(args[3]);
-            StringBuilder result = new StringBuilder();
-            for(Path path:paths){
-                result.append(path.toString());
-                result.append(":");
-                result.append( Files.readAllLines(path).size());
-                result.append( '\n');
-            }
-            Files.write(outfile, result.toString().getBytes());
+  public static void main(String[] args) throws IOException {
+    ImmutableList<Path> paths = ImmutableList.of(Paths.get(args[0]), Paths.get(args[1]), Paths.get(args[2]));
+    Path outfile = Paths.get(args[3]);
+    StringBuilder result = new StringBuilder();
+    for(Path path:paths){
+      result.append(path.toString());
+      result.append(":");
+      result.append( Files.readAllLines(path).size());
+      result.append( '\n');
     }
+    Files.write(outfile, result.toString().getBytes());
+  }
 }
+
