8c29c64e6b5b8177e8a420f91dcf25f86809a0b2
Gal Golan <galgolo@gmail.com>
2019-04-20 22:07:29 +0300

    Fix-formatting v2

--- projects/noloan2/app/src/main/java/hasadna/noloan2/SmsRecyclerAdapter.java
+++ projects/noloan2/app/src/main/java/hasadna/noloan2/SmsRecyclerAdapter.java
@@ -14,9 +14,9 @@ import hasadna.noloan2.protobuf.SmsProto.SmsMessage;
 import noloan.R;
 
 public class SmsRecyclerAdapter extends RecyclerView.Adapter<SmsRecyclerAdapter.RecyclerViewHolder> {
-  
+
   List<SmsMessage> messages;
-  
+
   public SmsRecyclerAdapter(List<SmsMessage> messages) {
     if (messages.size() == 0) {
       this.messages = new ArrayList<>();
@@ -25,36 +25,36 @@ public class SmsRecyclerAdapter extends RecyclerView.Adapter<SmsRecyclerAdapter.
     } else {
       this.messages = messages;
     }
-    
+
   }
-  
+
   @NonNull
   @Override
   public RecyclerViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
     LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
     return new RecyclerViewHolder(inflater.inflate(R.layout.messages_list_item, viewGroup, false));
   }
-  
+
   @Override
   public void onBindViewHolder(@NonNull RecyclerViewHolder recyclerViewHolder, int i) {
     recyclerViewHolder.bind(messages.get(i));
   }
-  
+
   @Override
   public int getItemCount() {
     return messages.size();
   }
-  
-  
+
+
   public class RecyclerViewHolder extends RecyclerView.ViewHolder {
     TextView from, content;
-    
+
     public RecyclerViewHolder(@NonNull View itemView) {
       super(itemView);
       from = itemView.findViewById(R.id.received_from);
       content = itemView.findViewById(R.id.content);
     }
-    
+
     public void bind(SmsMessage sms) {
       from.setText(sms.getSender());
       content.setText(sms.getBody());
