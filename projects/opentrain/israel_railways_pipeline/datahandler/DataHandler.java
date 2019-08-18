41c79d4d9e8fd47582071bbd39e259e4d4d3ab36
Jenia <jzalomsky93@gmail.com>
2019-08-18 14:09:48 +0300

    pipeline: routes-samples-stops-trips

--- projects/opentrain/israel_railways_pipeline/datahandler/DataHandler.java
+++ projects/opentrain/israel_railways_pipeline/datahandler/DataHandler.java
@@ -18,187 +18,109 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 class DataHandler {
-//
-//  public static List<Integer> getRouteIds() {
-//    return getRoutes(routesAbsPath).stream().map(Route::getRouteId).collect(Collectors.toList());
-//  }
-//
-//  public static List<Route> getRoutes(Path csvPath) {
 
-//  }
-//
-//  public static List<Integer> getStopsId() {
-//    return getStopTimes(stopTimesAbsPath)
-//        .stream()
-//        .map(StopTime::getStopId)
-//        .collect(Collectors.toList());
-//  }
-//
-//  public static List<StopTime> getStopTimes(Path csvPath) {
-//    List<String> tripsId = getTripsId();
-//    List<StopTime> stopsTime = new ArrayList<>();
-//    try {
-//      CSVParser parser =
-//          CSVParser.parse(
-//              csvPath,
-//              StandardCharsets.UTF_8,
-//              CSVFormat.DEFAULT.withHeader(
-//                  "trip_id",
-//                  "arrival_time",
-//                  "departure_time",
-//                  "stop_id",
-//                  "stop_sequence",
-//                  "pickup_type",
-//                  "drop_off_type",
-//                  "shape_dist_traveled"));
-//      boolean isHeader = true;
-//      for (CSVRecord record : parser) {
-//        if (isHeader) {
-//          isHeader = false;
-//          continue;
-//        }
-//        if (!tripsId.contains(record.get("trip_id"))) {
-//          continue;
-//        }
-//        StopTime.Builder stopTime = StopTime.newBuilder();
-//        stopsTime.add(
-//            stopTime
-//                .setTripId(record.get("trip_id"))
-//                .setArrivalTime(record.get("arrival_time"))
-//                .setDepartureTime(record.get("departure_time"))
-//                .setStopId(Integer.parseInt(record.get("stop_id")))
-//                .setStopSequence(record.get("stop_sequence"))
-//                .setPickupType(record.get("pickup_type"))
-//                .setDropOffType(record.get("drop_off_type"))
-//                .setShapeDistTraveled(record.get("shape_dist_traveled"))
-//                .build());
-//      }
-//      return stopsTime;
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
-//  public static List<Stop> getStops(Path csvPath) {
-//    List<Integer> stopsId = getStopsId();
-//    List<Stop> stops = new ArrayList<>();
-//    try {
-//      CSVParser parser =
-//          CSVParser.parse(
-//              csvPath,
-//              StandardCharsets.UTF_8,
-//              CSVFormat.DEFAULT.withHeader(
-//                  "stop_id",
-//                  "stop_code",
-//                  "stop_name",
-//                  "stop_desc",
-//                  "stop_lat",
-//                  "stop_lon",
-//                  "location_type",
-//                  "parent_station",
-//                  "zone_id"));
-//      boolean isHeader = true;
-//      for (CSVRecord record : parser) {
-//        if (isHeader) {
-//          isHeader = false;
-//          continue;
-//        }
-//        if (!stopsId.contains(Integer.parseInt(record.get("stop_id")))) {
-//          continue;
-//        }
-//        Stop.Builder stop = Stop.newBuilder();
-//        stops.add(
-//            stop.setStopId(Integer.parseInt(record.get("stop_id")))
-//                .setStopCode(Integer.parseInt(record.get("stop_code")))
-//                .setStopName(record.get("stop_name"))
-//                .setStopDesc(record.get("stop_desc"))
-//                .setStopLat(Double.parseDouble(record.get("stop_lat")))
-//                .setStopLon(Double.parseDouble(record.get("stop_lon")))
-//                .setLocationType(Integer.parseInt(record.get("location_type")))
-//                .setParentStation(record.get("parent_station"))
-//                .setZoneId(Integer.parseInt(record.get("zone_id")))
-//                .build());
-//      }
-//      return stops;
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
-//  public static List<Translation> getTranslations(Path csvPath) {
-//    List<Translation> translations = new ArrayList<>();
-//    try {
-//      CSVParser parser =
-//          CSVParser.parse(
-//              csvPath,
-//              StandardCharsets.UTF_8,
-//              CSVFormat.DEFAULT.withQuote(null).withHeader("trans_id", "lang", "translation"));
-//      boolean isHeader = true;
-//      for (CSVRecord record : parser) {
-//        if (isHeader) {
-//          isHeader = false;
-//          continue;
-//        }
-//        Translation.Builder translation = Translation.newBuilder();
-//        translations.add(
-//            translation
-//                .setTransId(record.get("trans_id"))
-//                .setLang(record.get("lang"))
-//                .setTranslation(record.get("translation"))
-//                .build());
-//      }
-//      return translations;
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
-//  public static List<String> getTripsId() {
-//    return getTrips(tripsAbsPath).stream().map(Trip::getTripId).collect(Collectors.toList());
-//  }
-//
-//  public static List<Trip> getTrips(Path csvPath) {
-//    List<Trip> trips = new ArrayList<>();
-//    List<Integer> routesId = getRouteIds();
-//    List<Integer> serviceIds = getServiceIds();
-//    try {
-//      CSVParser parser =
-//          CSVParser.parse(
-//              csvPath,
-//              StandardCharsets.UTF_8,
-//              CSVFormat.DEFAULT.withHeader(
-//                  "route_id",
-//                  "service_id",
-//                  "trip_id",
-//                  "trip_headsign",
-//                  "direction_id",
-//                  "shape_id"));
-//      boolean isHeader = true;
-//      for (CSVRecord record : parser) {
-//        if (isHeader) {
-//          isHeader = false;
-//          continue;
-//        }
-//        if (!routesId.contains(Integer.parseInt(record.get("route_id")))
-//            || !serviceIds.contains(Integer.parseInt(record.get("service_id")))) {
-//          continue;
-//        }
-//        Trip.Builder trip = Trip.newBuilder();
-//        trips.add(
-//            trip.setRouteId(Integer.parseInt(record.get("route_id")))
-//                .setServiceId(Integer.parseInt(record.get("service_id")))
-//                .setTripId(record.get("trip_id"))
-//                .setTripHeadsign(record.get("trip_headsign"))
-//                .setDirectionId(Integer.parseInt(record.get("direction_id")))
-//                .setShapeId(record.get("shape_id"))
-//                .build());
-//      }
-//      return trips;
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
+    public static void saveSamples(Path path, String outputName) {
+        try (FileOutputStream output = new FileOutputStream(outputName)) {
+            for (RailWayProtos.Samples sample : getSamples(path)) {
+                sample.writeDelimitedTo(output);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static List<RailWayProtos.Samples> getSamples(Path stopCsv) {
+        List<RailWayProtos.Samples> sampleList = new ArrayList<>();
+        try {
+            CSVParser parser =
+                    CSVParser.parse(
+                            stopCsv,
+                            StandardCharsets.UTF_8,
+                            CSVFormat.DEFAULT.withHeader("id","gtfs_stop_id","is_source","is_dest","actual_arrival","exp_arrival",
+                                    "actual_departure","exp_departure","delay_arrival","delay_departure","index","filename","line_number","valid","invalid_reason",
+                                    "stop_id","trip_id","sheet_idx","actual_departure_fixed","actual_arrival_fixed","ignored_error"));
+            boolean isHeader = true;
+            for (CSVRecord record : parser) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+
+                RailWayProtos.Samples.Builder sampleBuilder = RailWayProtos.Samples.newBuilder();
+                sampleList.add(sampleBuilder
+                                .setId(Integer.parseInt(record.get("id")))
+                                .setGtfsStopId(Integer.parseInt(record.get("gtfs_stop_id")))
+                                .setIsSource(Boolean.parseBoolean(record.get("is_source")))
+                                .setIsDest(Boolean.parseBoolean(record.get("is_dest")))
+                                .setActualArrival(record.get("actual_arrival"))
+                                .setExpArrival(record.get("exp_arrival"))
+                                .setActualDeparture(record.get("actual_departure"))
+                                .setExpDeparture(record.get("exp_departure"))
+                                .setDelayArrival(record.get("delay_arrival"))
+                                .setDelayDeparture(record.get("delay_departure"))
+                                .setIndex(Integer.parseInt(record.get("index")))
+                                .setFilename(record.get("filename"))
+                                .setLineNumber(Integer.parseInt(record.get("line_number")))
+                                .setValid(Boolean.parseBoolean(record.get("valid")))
+                                .setInvalidReason(record.get("invalid_reason"))
+                                .setStopId(Integer.parseInt(record.get("stop_id")))
+                                .setTripId(Integer.parseInt(record.get("trip_id")))
+                                .setSheetIdx(record.get("sheet_idx"))
+                                .setActualDepartureFixed(record.get("actual_departure_fixed"))
+                                .setActualArrivalFixed(record.get("actual_arrival_fixed"))
+                                .setIgnoredError(record.get("ignored_error"))
+                                .build());
+            }
+            return sampleList;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static void saveStops(Path path, String outputName) {
+        try (FileOutputStream output = new FileOutputStream(outputName)) {
+            for (RailWayProtos.Stops stop : getStops(path)) {
+                stop.writeDelimitedTo(output);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public static List<RailWayProtos.Stops> getStops(Path stopCsv) {
+        List<RailWayProtos.Stops> stopsList = new ArrayList<>();
+        try {
+            CSVParser parser =
+                    CSVParser.parse(
+                            stopCsv,
+                            StandardCharsets.UTF_8,
+                            CSVFormat.DEFAULT.withHeader("id","gtfs_stop_id","english","hebrews","lat","lon"));
+            boolean isHeader = true;
+            for (CSVRecord record : parser) {
+                if (isHeader) {
+                    isHeader = false;
+                    continue;
+                }
+
+                RailWayProtos.Stops.Builder stopBuilder = RailWayProtos.Stops.newBuilder();
+                stopsList.add(
+                        stopBuilder
+                                .setId(Integer.parseInt(record.get("id")))
+                                .setGtfsStopId(record.get("gtfs_stop_id"))
+                                .setEnglish(record.get("english"))
+                                .setHebrews(record.get("hebrews"))
+                                .setLat(record.get("lat"))
+                                .setLon(record.get("lon"))
+                                .build());
+            }
+            return stopsList;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
    public static void saveTrips(Path path, String outputName) {
@@ -213,7 +135,6 @@ class DataHandler {
 
 
     public static List<RailWayProtos.Trips> getTrips(Path tripsCsv) {
-
         List<RailWayProtos.Trips> tripsList = new ArrayList<>();
         try {
             CSVParser parser =
@@ -232,20 +153,19 @@ class DataHandler {
 
                 RailWayProtos.Trips.Builder tripBuilder = RailWayProtos.Trips.newBuilder();
                 tripsList.add(
-                        tripBuilder
-                                .setId(Integer.parseInt(record.get("id")))
-                                .setTrainNum(Integer.parseInt(record.get("train_num")))
+                        tripBuilder.setId(record.get("id"))
+                                .setTrainNum(record.get("train_num"))
                                 .setDate(record.get("date"))
-                                .setValid(Boolean.parseBoolean(record.get("valid")))
+                                .setValid(record.get("valid"))
                                 .setInvalidReason(record.get("invalid_reason"))
                                 .setXWeekDayLocal(record.get("x_week_day_local"))
                                 .setXHourLocal(record.get("x_hour_local"))
-                                .setRouteId(Integer.parseInt(record.get("route_id")))
+                                .setRouteId(record.get("route_id"))
                                 .setXAvgDelayArrival(record.get("x_avg_delay_arrival"))
                                 .setXCacheVersion(record.get("x_cache_version"))
                                 .setXMax2DelayArrival(record.get("x_max2_delay_arrival"))
                                 .setXBeforeLastDelayArrival(record.get("x_before_last_delay_arrival"))
-                                .setXLastDelayArrival(Integer.parseInt(record.get("setXLastDelayArrival")))   //error
+                                .setXLastDelayArrival(record.get("setXLastDelayArrival"))   //error
                                 .build());
             }
             return tripsList;
@@ -298,27 +218,6 @@ class DataHandler {
     }
 
 
-
-//  public static void saveStops() {
-//    try (FileOutputStream output = new FileOutputStream(stopsProtoAbsPath)) {
-//      for (Stop stop : getStops(stopsAbsPath)) {
-//        stop.writeDelimitedTo(output);
-//      }
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
-//
-
-//  public static void saveTrips() {
-//    try (FileOutputStream output = new FileOutputStream(tripsProtoAbsPath)) {
-//      for (Trip trip : getTrips(tripsAbsPath)) {
-//        trip.writeDelimitedTo(output);
-//      }
-//    } catch (IOException e) {
-//      throw new RuntimeException(e);
-//    }
-//  }
 //
 //  // Paths to the CSVs files
 //  static Path routesAbsPath;
@@ -333,6 +232,8 @@ class DataHandler {
 //        getRoutes(Paths.get(args[0]));
 //        System.out.println(getRoutes(Paths.get(args[0])));
         saveRoutes(Paths.get(args[0]), args[1]);
+        saveSamples(Paths.get(args[6]),(args[7]));
+        saveStops(Paths.get(args[4]), args[5]);
         saveTrips(Paths.get(args[2]), args[3]);
 
     }
