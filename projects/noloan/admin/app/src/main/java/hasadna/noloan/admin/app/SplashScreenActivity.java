be97f4e239a8abdb89d93721fed2b80094f7c940
Gulilo <galgolo@gmail.com>
2019-08-26 13:36:23 +0300

    Coping code for the main app

---
+++ projects/noloan/admin/app/src/main/java/hasadna/noloan/admin/app/SplashScreenActivity.java
@@ -0,0 +1,148 @@
+package hasadna.noloan.admin.app;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.android.gms.tasks.Tasks;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import hasadna.noloan.admin.app.firestore.FirestoreClient;
+import hasadna.noloan.protobuf.SmsProto.SpamList;
+import noloan.R;
+
+// Permission request Based on
+// http://pcessflight.com/smart-android-splash-screen-grabbing-permissions/
+public class SplashScreenActivity extends AppCompatActivity {
+
+  static final long SPLASH_TIME_MS = 1000;
+  private static final int PERMISSION_REQUEST_CODE = 123;
+  final String[] requiredPermissions = new String[] {Manifest.permission.READ_SMS};
+
+  Handler handler;
+
+  TaskCompletionSource<Boolean> permissionTask;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash_permission);
+
+    handler = new Handler(Looper.getMainLooper());
+
+    permissionTask = new TaskCompletionSource<>();
+    SplashTask task = new SplashTask();
+    task.execute();
+  }
+
+  private void checkPermissions() {
+    String[] ungrantedPermissions = permissionsStillNeeded();
+    if (ungrantedPermissions.length == 0) {
+      permissionTask.setResult(true);
+    } else {
+      requestPermissions(ungrantedPermissions, PERMISSION_REQUEST_CODE);
+    }
+  }
+
+  private String[] permissionsStillNeeded() {
+    ArrayList<String> result = new ArrayList<>();
+    for (String permission : requiredPermissions) {
+      if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+        result.add(permission);
+      }
+    }
+    return result.toArray(new String[result.size()]);
+  }
+
+  @Override
+  public void onRequestPermissionsResult(
+      int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    if (requestCode == PERMISSION_REQUEST_CODE) {
+      if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        permissionTask.setResult(true);
+      } else {
+        permissionTask.setResult(false);
+      }
+    }
+  }
+
+  // Get the spam from the Firestore
+  private Task<DocumentSnapshot> getSpam() {
+    FirestoreClient client = new FirestoreClient();
+    Executor executor = Executors.newSingleThreadExecutor();
+    Task<DocumentSnapshot> task = client.getSpamTask();
+    task.addOnCompleteListener(
+        executor,
+        task1 -> {
+          if (task1.isSuccessful()) {
+            DocumentSnapshot snapshot = task1.getResult();
+            try {
+              SpamList spam =
+                  (SpamList)
+                      client.decodeMessage(snapshot.getString("proto"), SpamList.newBuilder());
+              SpamHolder.getInstance().setSpam(spam.getSmsList());
+            } catch (InvalidProtocolBufferException e) {
+              Log.e("SplashScreen", "Decoding spam failed");
+            }
+          }
+        });
+    return task;
+  }
+
+  // Start the main Activity
+  private void startNextActivity() {
+    handler.postDelayed(
+        () -> {
+          startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+          finish();
+        },
+        SPLASH_TIME_MS);
+  }
+
+  // Main Task to get the permissions and the spam
+  private class SplashTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+      checkPermissions();
+      Task<DocumentSnapshot> spam = getSpam();
+      Task<Boolean> permissions = permissionTask.getTask();
+      try {
+
+        Tasks.await(permissions);
+        Tasks.await(spam);
+      } catch (ExecutionException | InterruptedException e) {
+        e.printStackTrace();
+      }
+      if (!permissions.getResult()) {
+        handler.post(
+            () ->
+                Toast.makeText(
+                        getApplicationContext(),
+                        "This App require SMS reading To work",
+                        Toast.LENGTH_SHORT)
+                    .show());
+        finishAndRemoveTask();
+      } else {
+        startNextActivity();
+      }
+      return null;
+    }
+  }
+}
+
