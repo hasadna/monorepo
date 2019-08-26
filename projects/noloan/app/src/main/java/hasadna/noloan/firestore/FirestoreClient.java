863f303d88d090265427913161f848b460dd42e4
Gulilo <galgolo@gmail.com>
2019-08-26 13:58:14 +0300

    cleaning

--- projects/noloan/app/src/main/java/hasadna/noloan/firestore/FirestoreClient.java
+++ projects/noloan/app/src/main/java/hasadna/noloan/firestore/FirestoreClient.java
@@ -3,19 +3,24 @@ package hasadna.noloan.firestore;
 import android.util.Base64;
 
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.android.gms.tasks.TaskCompletionSource;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.MessageLite;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+import hasadna.noloan.SpamHolder;
 import hasadna.noloan.protobuf.SmsProto.SmsMessage;
 
 public class FirestoreClient {
-  private final String SPAM_DOCUMENT_PATH = "noloan/smss";
+  public static final String SPAM_COLLECTION_PATH = "noloan/Spam/smss";
 
-  // < - 22 > - 31 for removing and changing with the user name
-  private final String USER_SUGGEST_COLLECTION = "noloan/user_data/user/<username>/spam_suggestion";
+  public static final String USER_SUGGEST_COLLECTION = "noloan/user_data/user/<username>/spam_suggestion";
 
   private FirebaseFirestore client;
 
@@ -23,17 +28,46 @@ public class FirestoreClient {
     client = FirebaseFirestore.getInstance();
   }
 
-  public void writeMessage(SmsMessage message) {
+  public void writeMessage(SmsMessage message, String path) {
     FirestoreElement element = encodeMessage(message);
-
-    // TODO add code to remove the <username> from the path and add the actual user name
-
-    client.collection(USER_SUGGEST_COLLECTION).add(element);
+    client.collection(path).add(element);
   }
 
-  public Task<DocumentSnapshot> getSpamTask() {
-    DocumentReference documentReference = client.document(SPAM_DOCUMENT_PATH);
-    return documentReference.get();
+  // Start real-time listening to the DB for change, return set the result to true when done.
+  public TaskCompletionSource StartListeningSpam() {
+    Executor executor = Executors.newSingleThreadExecutor();
+    TaskCompletionSource task = new TaskCompletionSource<> ();
+
+    CollectionReference collectionReference = client.collection(SPAM_COLLECTION_PATH);
+    collectionReference.addSnapshotListener(executor, (queryDocumentSnapshots, e) ->
+    {
+      if (e != null) {
+        return;
+      }
+
+      SpamHolder sp = SpamHolder.getInstance();
+      for (DocumentChange dc : queryDocumentSnapshots.getDocumentChanges()) {
+        SmsMessage sms = null;
+        try {
+          sms = (SmsMessage) decodeMessage(dc.getDocument().getString("proto"), SmsMessage.newBuilder());
+        } catch (InvalidProtocolBufferException e1) {
+          e1.printStackTrace();
+        }
+        switch (dc.getType()) {
+          case ADDED:
+            sp.add(sms);
+            break;
+          case MODIFIED:
+            sp.modified(sms);
+            break;
+          case REMOVED:
+            sp.remove(sms);
+            break;
+        }
+      }
+      task.setResult(true);
+    });
+    return task;
   }
 
   // Encode user proto to base64 for storing in Firestore
