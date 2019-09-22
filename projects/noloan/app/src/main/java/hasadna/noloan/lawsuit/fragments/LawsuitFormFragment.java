bc677781c77ec8e804492d8b5633ed8006e1a33f
Tom <cotom972@gmail.com>
2019-09-22 19:26:53 +0300

    Add: user id validation to form, labels for editTexts

--- projects/noloan/app/src/main/java/hasadna/noloan/lawsuit/fragments/LawsuitFormFragment.java
+++ projects/noloan/app/src/main/java/hasadna/noloan/lawsuit/fragments/LawsuitFormFragment.java
@@ -2,6 +2,7 @@ package hasadna.noloan.lawsuit.fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,9 +22,12 @@ public class LawsuitFormFragment extends Fragment {
 
   LawsuitActivity lawsuitActivity;
   EditText receivedDate;
+  TextInputEditText userId;
   DatePickerDialog datePickerDialog;
   Calendar calendar;
   Button confirmForm;
+
+  private boolean formValidated = false;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -37,10 +41,21 @@ public class LawsuitFormFragment extends Fragment {
     ViewGroup rootView =
         (ViewGroup) inflater.inflate(R.layout.lawsuit_fragment_form, container, false);
 
+    userId = rootView.findViewById(R.id.EditText_userID);
     receivedDate = rootView.findViewById(R.id.EditText_receivedSpamDate);
-    confirmForm = rootView.findViewById(R.id.button_formFragment_next);
+    confirmForm = rootView.findViewById(R.id.Button_formFragment_next);
 
-    // If court was already selected, display its name
+    // Validate user id number
+    userId.setOnFocusChangeListener(
+        (v, hasFocus) -> {
+          if (userId.getText().length() != 0 && !validateIdNumber(userId.getText().toString())) {
+            rootView.findViewById(R.id.textView_idValidationError).setVisibility(View.VISIBLE);
+            formValidated = false;
+          } else
+            rootView.findViewById(R.id.textView_idValidationError).setVisibility(View.INVISIBLE);
+          formValidated = true;
+        });
+    // Display court's detail in TextViews
     receivedDate.setText(lawsuitActivity.selectedSmsSpam.getReceivedAt());
     receivedDate.setOnClickListener(v -> displayDatePicker());
     // Create PDF and proceed to SummaryFragment (Get file's path with:
@@ -79,36 +94,66 @@ public class LawsuitFormFragment extends Fragment {
         Lawsuit.newBuilder()
             // User
             .setFirstName(
-                ((EditText) getView().findViewById(R.id.userFirstName)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_userFirstName))
+                    .getText()
+                    .toString())
             .setLastName(
-                ((EditText) getView().findViewById(R.id.userLastName)).getText().toString())
-            .setUserId(((EditText) getView().findViewById(R.id.userID)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_userLastName))
+                    .getText()
+                    .toString())
+            .setUserId(
+                ((EditText) getView().findViewById(R.id.EditText_userID)).getText().toString())
             .setUserAddress(
-                ((EditText) getView().findViewById(R.id.userAddress)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_userAddress)).getText().toString())
             .setUserPhone(((EditText) getView().findViewById(R.id.userPhone)).getText().toString())
 
             // Company
             .setCompanyName(
-                ((EditText) getView().findViewById(R.id.companyName)).getText().toString())
-            .setCompanyId(((EditText) getView().findViewById(R.id.companyId)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_companyName)).getText().toString())
+            .setCompanyId(
+                ((EditText) getView().findViewById(R.id.EditText_companyId)).getText().toString())
             .setCompanyAddress(
-                ((EditText) getView().findViewById(R.id.companyAddress)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_companyAddress))
+                    .getText()
+                    .toString())
             .setCompanyPhone(
-                ((EditText) getView().findViewById(R.id.companyPhone)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_companyPhone))
+                    .getText()
+                    .toString())
             .setCompanyFax(
-                ((EditText) getView().findViewById(R.id.companyFax)).getText().toString())
+                ((EditText) getView().findViewById(R.id.EditText_companyFax)).getText().toString())
 
             // General
             .setDateReceived(
                 ((EditText) getView().findViewById(R.id.EditText_receivedSpamDate))
                     .getText()
                     .toString())
-            .setSentHaser(((CheckBox) getView().findViewById(R.id.checkBox_sentHaser)).isChecked())
+            .setSentHaser(((CheckBox) getView().findViewById(R.id.CheckBox_sentHaser)).isChecked())
             .setMoreThanFiveClaims(
-                ((CheckBox) getView().findViewById(R.id.checkbox_fiveLawsuits)).isChecked())
+                ((CheckBox) getView().findViewById(R.id.CheckBox_fiveLawsuits)).isChecked())
             .setClaimAmount(
                 ((EditText) getView().findViewById(R.id.EditText_claimAmount)).getText().toString())
             .build();
+  }
+
+  private boolean validateIdNumber(String idNumber) {
+
+    if (idNumber.length() > 9) return false;
+
+    // Complement id number to 9 digits
+    int[] id = new int[9];
+    for (int i = 0; i < idNumber.length(); i++) {
+      id[8 - i] = Integer.parseInt("" + idNumber.charAt(idNumber.length() - 1 - i));
+    }
+    // Validate sums of digits - https://www.excelmaster.co.il/2018/08/06/check_id_excel/
+    int sum = 0;
+    for (int i = 1; i < 9; i = i + 2) {
+      id[i] = ((id[i] * 2) > 9) ? ((id[i] * 2) % 10 + (id[i] * 2) / 10) : id[i] * 2;
+      sum += id[i - 1] + id[i];
+    }
+    sum += id[8];
+    if (sum % 10 == 0) return true;
+    else return false;
   }
 }
 
