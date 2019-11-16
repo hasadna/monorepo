102ba0c4051bd0498205f123de3e129679c89a33
Gal Golan <gulilo@users.noreply.github.com>
2019-11-16 20:52:30 +0200

    Noloan - admin login

---
+++ projects/noloan/admin/src/main/java/hasadna/noloan/admin/app/LoginActivity.java
@@ -0,0 +1,54 @@
+package hasadna.noloan.admin.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import hasadna.noloan.common.FirebaseAuthentication;
+
+// TODO Read about FirebaseUI
+public class LoginActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_login);
+    // In case the admin is already logged in
+    if (FirebaseAuthentication.getInstance().isSignedIn()) {
+      startActivity(new Intent(LoginActivity.this, SplashScreenActivity.class));
+      finish();
+    } else {
+      TextView emailText = findViewById(R.id.email_Text);
+      TextView passwordText = findViewById(R.id.password_text);
+
+      Button login = findViewById(R.id.login_button);
+      login.setOnClickListener(
+          v -> {
+            String email = emailText.getText().toString();
+            String password = passwordText.getText().toString();
+            if (!email.isEmpty() && !password.isEmpty()) {
+              FirebaseAuthentication.getInstance()
+                  .signInAdmin(email, password)
+                  .getTask()
+                  .addOnCompleteListener(
+                      task -> {
+                        if (task.isSuccessful()) {
+                          startActivity(new Intent(LoginActivity.this, SplashScreenActivity.class));
+                          finish();
+                        } else {
+                          Toast.makeText(getBaseContext(), "Failed to login", Toast.LENGTH_LONG)
+                              .show();
+                        }
+                      });
+            } else {
+              Toast.makeText(this, "Email or password is empty", Toast.LENGTH_LONG).show();
+            }
+          });
+    }
+  }
+}
+
--- projects/noloan/common/src/main/java/hasadna/noloan/common/FirebaseAuthentication.java
+++ projects/noloan/common/src/main/java/hasadna/noloan/common/FirebaseAuthentication.java
@@ -2,6 +2,7 @@ package hasadna.noloan.common;
 
 import android.util.Log;
 
+import com.google.android.gms.tasks.TaskCompletionSource;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -20,8 +21,12 @@ public class FirebaseAuthentication {
     return instance;
   }
 
-  public void signinAnonymusly() {
+  public FirebaseAuthentication() {
     auth = FirebaseAuth.getInstance();
+    user = auth.getCurrentUser();
+  }
+
+  public void signInAnonymusly() {
     auth.signInAnonymously()
         .addOnCompleteListener(
             task -> {
@@ -33,7 +38,43 @@ public class FirebaseAuthentication {
             });
   }
 
+  public TaskCompletionSource<Boolean> signInAdmin(String email, String password) {
+    Log.d(TAG, "signInAdmin starting");
+    TaskCompletionSource result = new TaskCompletionSource<>();
+
+    // If not already signed in.
+    if (user == null) {
+      auth.signInWithEmailAndPassword(email, password)
+          .addOnCompleteListener(
+              task -> {
+                if (task.isSuccessful()) {
+                  Log.d(TAG, "Login admin successfully");
+                  user = auth.getCurrentUser();
+                  result.trySetResult(true);
+                } else {
+                  Log.e(TAG, "Failed to signin admin" + email + "  " + password);
+                  result.trySetResult(false);
+                }
+              });
+    } else {
+      result.trySetResult(true);
+    }
+    return result;
+  }
+
+  public void signOut() {
+    auth.signOut();
+    user = null;
+  }
+
+  public boolean isSignedIn() {
+    return user != null;
+  }
+
   public String getCurrentUserId() {
+    if (user == null) {
+      return null;
+    }
     return user.getUid();
   }
 }
